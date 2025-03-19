025fe707c9f12cdb92898f93329fa382460e4750
ulysse <ulysse@alenvi.io>
2025-03-19 16:18:32 +0100

    COM-3928: coach

--- src/core/components/table/CoachList.vue
+++ src/core/components/table/CoachList.vue
@@ -31,21 +31,23 @@
     </div>
 
     <coach-creation-modal v-model="coachCreationModal" v-model:new-coach="newCoach" :validations="v$.newCoach"
-      :first-step="firstStep" :loading="loading" :phone-nbr-error="phoneNbrError(v$.newCoach)"
+      :first-step="firstStep" :loading="loading" :phone-nbr-error="phoneNbrError(v$.newCoach.contact)"
       :role-options="roleOptions" @hide="resetCoachCreationForm" @show="openCoachCreationModal" @submit="createCoach"
       @go-to-next-step="nextStep" :email-error="emailError(v$.newCoach)" />
 
-    <coach-edition-modal v-model="coachEditionModal" :phone-nbr-error="phoneNbrError(v$.selectedCoach)"
+    <coach-edition-modal v-model="coachEditionModal" :phone-nbr-error="phoneNbrError(v$.selectedCoach.contact)"
       :validations="v$.selectedCoach" :email-error="emailError(v$.selectedCoach)" v-model:selected-coach="selectedCoach"
       :role-options="roleOptions" @hide="resetCoachEditionForm" @submit="updateCoach" :loading="loading" />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { toRefs, ref, computed } from 'vue';
+import { useStore } from 'vuex';
 import useVuelidate from '@vuelidate/core';
 import { required, email } from '@vuelidate/validators';
 import get from 'lodash/get';
+import has from 'lodash/has';
 import pick from 'lodash/pick';
 import pickBy from 'lodash/pickBy';
 import omit from 'lodash/omit';
@@ -59,11 +61,11 @@ import ResponsiveTable from '@components/table/ResponsiveTable';
 import CoachCreationModal from '@components/table/CoachCreationModal';
 import CoachEditionModal from '@components/table/CoachEditionModal';
 import { NotifyNegative, NotifyWarning, NotifyPositive } from '@components/popup/notify';
-import { userMixin } from '@mixins/userMixin';
+import { useUser } from '@composables/user';
 import { formatPhone, clear, removeEmptyProps, formatPhoneForPayload } from '@helpers/utils';
 import { defineAbilitiesFor } from '@helpers/ability';
 import { ROLES_TRANSLATION, CLIENT_ADMIN, COACH } from '@data/constants';
-import { frPhoneNumber } from '@helpers/vuelidateCustomVal';
+import { frPhoneNumber, countryCode } from '@helpers/vuelidateCustomVal';
 
 export default {
   name: 'CoachList',
@@ -76,96 +78,103 @@ export default {
   props: {
     company: { type: Object, default: () => ({}) },
   },
-  mixins: [userMixin],
-  setup () { return { v$: useVuelidate() }; },
-  data () {
-    return {
-      loading: false,
-      usersLoading: false,
-      coachCreationModal: false,
-      coachEditionModal: false,
-      firstStep: true,
-      usersColumns: [
-        { name: 'firstname', label: 'Prénom', align: 'left', field: row => get(row, 'identity.firstname') || '' },
-        { name: 'lastname', label: 'Nom', align: 'left', field: row => get(row, 'identity.lastname') || '' },
-        { name: 'email', label: 'Email', align: 'left', field: row => get(row, 'local.email') || '' },
-        {
-          name: 'phone',
-          label: 'Téléphone',
-          align: 'left',
-          field: row => get(row, 'contact.phone') || '',
-          format: formatPhone,
-        },
-        {
-          name: 'role',
-          label: 'Role',
-          align: 'left',
-          field: row => get(row, 'role.client.name') || '',
-          format: value => (value ? ROLES_TRANSLATION[value] : ''),
-        },
-        { name: 'actions', label: '', align: 'center' },
-      ],
-      usersPagination: { rowsPerPage: 0, sortBy: 'lastname' },
-      newCoach: {
-        identity: { firstname: '', lastname: '' },
-        contact: { phone: '' },
-        role: '',
-        local: { email: '' },
-        company: '',
-        sendEmail: true,
+  setup (props) {
+    const $store = useStore();
+
+    const { company } = toRefs(props);
+
+    const loading = ref(false);
+    const usersLoading = ref(false);
+    const coachCreationModal = ref(false);
+    const coachEditionModal = ref(false);
+    const firstStep = ref(true);
+    const usersColumns = ref([
+      { name: 'firstname', label: 'Prénom', align: 'left', field: row => get(row, 'identity.firstname') || '' },
+      { name: 'lastname', label: 'Nom', align: 'left', field: row => get(row, 'identity.lastname') || '' },
+      { name: 'email', label: 'Email', align: 'left', field: row => get(row, 'local.email') || '' },
+      {
+        name: 'phone',
+        label: 'Téléphone',
+        align: 'left',
+        field: row => get(row, 'contact.phone') || '',
+        format: formatPhone,
       },
-      userValidations: {
+      {
+        name: 'role',
+        label: 'Role',
+        align: 'left',
+        field: row => get(row, 'role.client.name') || '',
+        format: value => (value ? ROLES_TRANSLATION[value] : ''),
+      },
+      { name: 'actions', label: '', align: 'center' },
+    ]);
+    const usersPagination = ref({ rowsPerPage: 0, sortBy: 'lastname' });
+    const newCoach = ref({
+      identity: { firstname: '', lastname: '' },
+      contact: { phone: '', countryCode: '+33' },
+      role: '',
+      local: { email: '' },
+      company: '',
+      sendEmail: true,
+    });
+    const selectedCoach = ref({
+      identity: {},
+      local: {},
+      contact: {},
+    });
+    const roles = ref([]);
+    const users = ref([]);
+
+    const userRules = computed(() => ({
+      newCoach: {
         identity: { lastname: { required } },
         local: { email: { required, email } },
-        contact: { phone: { frPhoneNumber } },
+        contact: { phone: { frPhoneNumber }, countryCode: { countryCode } },
         role: { required },
       },
       selectedCoach: {
-        identity: {},
-        local: {},
-        contact: {},
+        identity: { lastname: { required } },
+        local: { email: { required, email } },
+        contact: { phone: { frPhoneNumber }, countryCode: { countryCode } },
+        role: { required },
       },
-      roles: [],
-      users: [],
-    };
-  },
-  validations () {
-    return {
-      newCoach: this.userValidations,
-      selectedCoach: this.userValidations,
-    };
-  },
-  async created () {
-    await this.getUsers();
-  },
-  computed: {
-    ...mapState('main', ['loggedUser']),
-    roleOptions () {
-      return this.roles.map(role => ({ label: ROLES_TRANSLATION[role.name], value: role._id }));
-    },
-    canSetUserCompany () {
-      const ability = defineAbilitiesFor(pick(this.loggedUser, ['role', 'company']));
+    }));
+
+    const v$ = useVuelidate(userRules, { newCoach, selectedCoach });
+
+    const loggedUser = computed(() => $store.state.main.loggedUser);
+
+    const { emailError, phoneNbrError } = useUser();
+
+    const roleOptions = computed(() => roles.value
+      .map(role => ({ label: ROLES_TRANSLATION[role.name], value: role._id })));
+
+    const canSetUserCompany = computed(() => {
+      const ability = defineAbilitiesFor(pick(loggedUser.value, ['role', 'company']));
 
       return ability.can('set', 'user_company');
-    },
-  },
-  methods: {
-    formatUserPayload (user) {
-      const userPayload = removeEmptyProps(user);
-      if (this.canSetUserCompany) userPayload.company = this.company._id;
-      if (get(user, 'contact.phone')) userPayload.contact.phone = formatPhoneForPayload(user.contact.phone);
+    });
 
+    const formatUserPayload = (user) => {
+      let userPayload = cloneDeep(user);
+      if (get(user, 'contact.phone')) userPayload.contact.phone = formatPhoneForPayload(user.contact.phone);
+      else userPayload.contact.countryCode = '';
+      userPayload = removeEmptyProps(userPayload);
+      if (canSetUserCompany.value) userPayload.company = company.value._id;
       return omit(userPayload, 'sendEmail');
-    },
-    async nextStep () {
+    };
+
+    const nextStep = async () => {
       try {
-        this.loading = true;
-        this.v$.newCoach.local.email.$touch();
-        if (this.v$.newCoach.local.email.$error || !this.newCoach.local.email) return NotifyWarning('Champs invalides');
-        const userInfo = await Users.exists({ email: this.newCoach.local.email });
+        loading.value = true;
+        v$.value.newCoach.local.email.$touch();
+        if (v$.value.newCoach.local.email.$error || !newCoach.value.local.email) {
+          return NotifyWarning('Champs invalides');
+        }
+        const userInfo = await Users.exists({ email: newCoach.value.local.email });
         const { user } = userInfo;
 
-        const sameOrNoCompany = !user.company || user.company === this.company._id;
+        const sameOrNoCompany = !user.company || user.company === company.value._id;
         const noDataOnUser = !Object.keys(user).length;
         if (userInfo.exists && (!sameOrNoCompany || noDataOnUser)) {
           return NotifyNegative('Ce compte n\'est pas relié à cette structure.');
@@ -173,125 +182,177 @@ export default {
         if (userInfo.exists && get(userInfo, 'user.role.client')) return NotifyNegative('Compte déjà existant.');
 
         if (userInfo.exists) {
-          const payload = { role: this.newCoach.role };
-          if (!user.company) await UserCompanies.create({ user: userInfo.user._id, company: this.company._id });
+          const payload = { role: newCoach.value.role };
+          if (!user.company) await UserCompanies.create({ user: userInfo.user._id, company: company.value._id });
 
           await Users.updateById(userInfo.user._id, payload);
 
           NotifyPositive('Compte créé.');
-          await this.getUsers();
-          this.coachCreationModal = false;
-        } else this.firstStep = false;
+          await getUsers();
+          coachCreationModal.value = false;
+        } else firstStep.value = false;
       } catch (e) {
         console.error(e);
         if (e.status === 409) return NotifyNegative(e.data.message);
         NotifyNegative('Erreur lors de la création du compte.');
       } finally {
-        this.loading = false;
+        loading.value = false;
       }
-    },
-    async sendEmail () {
+    };
+
+    const sendEmail = async () => {
       try {
-        const userRole = this.roles.find(role => role._id === this.newCoach.role);
+        const userRole = roles.value.find(role => role._id === newCoach.value.role);
         if (!get(userRole, 'name')) return NotifyNegative('Problème lors de l\'envoi du mail.');
 
-        await Email.sendWelcome({ email: this.newCoach.local.email, type: get(userRole, 'name') });
+        await Email.sendWelcome({ email: newCoach.value.local.email, type: get(userRole, 'name') });
         NotifyPositive('Email envoyé.');
       } catch (e) {
         console.error(e);
         NotifyNegative('Erreur lors de l\'envoi du mail.');
       }
-    },
-    async createCoach () {
-      try {
-        this.loading = true;
-        this.v$.newCoach.$touch();
-        if (this.v$.newCoach.$error) return NotifyWarning('Champ(s) invalide(s)');
+    };
 
-        await Users.create(this.formatUserPayload(this.newCoach));
+    const createCoach = async () => {
+      try {
+        loading.value = true;
+        v$.value.newCoach.$touch();
+        if (v$.value.newCoach.$error) return NotifyWarning('Champ(s) invalide(s)');
+
+        await Users.create(formatUserPayload(newCoach.value));
 
         NotifyPositive('Compte enregistré.');
       } catch (e) {
         console.error(e);
         NotifyNegative('Erreur lors de la création du compte.');
       } finally {
-        this.loading = false;
+        loading.value = false;
       }
 
-      if (this.newCoach.sendEmail) await this.sendEmail();
+      if (newCoach.value.sendEmail) await sendEmail();
 
-      await this.getUsers();
-      this.coachCreationModal = false;
-    },
-    resetCoachCreationForm () {
-      this.firstStep = true;
-      this.newCoach = { ...clear(this.newCoach), sendEmail: true };
-      this.v$.newCoach.$reset();
-    },
-    async getRoles () {
+      await getUsers();
+      coachCreationModal.value = false;
+    };
+
+    const resetCoachCreationForm = () => {
+      firstStep.value = true;
+      newCoach.value = { ...clear(newCoach.value), contact: { phone: '', countryCode: '+33' }, sendEmail: true };
+      v$.value.newCoach.$reset();
+    };
+
+    const getRoles = async () => {
       try {
-        this.roles = await Roles.list({ name: [CLIENT_ADMIN, COACH] });
+        roles.value = await Roles.list({ name: [CLIENT_ADMIN, COACH] });
       } catch (e) {
         console.error(e);
-        this.roles = [];
+        roles.value = [];
       }
-    },
-    async openCoachCreationModal () {
-      if (!this.roles.length) await this.getRoles();
-      const coachRole = this.roles.find(role => role.name === COACH);
-      if (coachRole) this.newCoach.role = coachRole._id;
-    },
-    async openCoachEditionModal (user) {
-      if (!this.roles.length) await this.getRoles();
+    };
 
-      this.selectedCoach = {
-        ...this.selectedCoach,
+    const openCoachCreationModal = async () => {
+      if (!roles.value.length) await getRoles();
+      const coachRole = roles.value.find(role => role.name === COACH);
+      if (coachRole) newCoach.value.role = coachRole._id;
+    };
+
+    const openCoachEditionModal = async (user) => {
+      if (!roles.value.length) await getRoles();
+      selectedCoach.value = {
+        ...selectedCoach.value,
+        ...(!get(user, 'contact.countryCode') && { contact: { countryCode: '+33' } }),
         role: user.role.client._id,
-        ...pick(cloneDeep(user), ['_id', 'identity.firstname', 'identity.lastname', 'local.email', 'contact.phone']),
+        ...pick(
+          cloneDeep(user),
+          ['_id', 'identity.firstname', 'identity.lastname', 'local.email', 'contact.phone', 'contact.countryCode']
+        ),
       };
-      this.coachEditionModal = true;
-    },
-    resetCoachEditionForm () {
-      this.v$.selectedCoach.$reset();
-      this.selectedCoach = { identity: {}, local: {}, contact: {} };
-    },
-    formatUpdatedUserPayload (user) {
+      coachEditionModal.value = true;
+    };
+
+    const resetCoachEditionForm = () => {
+      v$.value.selectedCoach.$reset();
+      selectedCoach.value = { identity: {}, local: {}, contact: {} };
+    };
+
+    const formatUpdatedUserPayload = (user) => {
       const userPayload = pickBy(
-        pick(user, ['identity.firstname', 'identity.lastname', 'local.email', 'role', 'contact.phone'])
+        pick(
+          user,
+          ['identity.firstname', 'identity.lastname', 'local.email', 'role', 'contact.phone', 'contact.countryCode']
+        )
       );
       if (get(user, 'contact.phone')) userPayload.contact.phone = formatPhoneForPayload(user.contact.phone);
+      else if (has(user, 'contact.phone')) userPayload.contact.countryCode = '';
+      else return omit(userPayload, 'contact.countryCode');
       return userPayload;
-    },
-    async updateCoach () {
-      try {
-        this.loading = true;
-        this.v$.selectedCoach.$touch();
-        if (this.v$.selectedCoach.$error) return NotifyWarning('Champ(s) invalide(s)');
+    };
 
-        await Users.updateById(this.selectedCoach._id, this.formatUpdatedUserPayload(this.selectedCoach));
-        this.coachEditionModal = false;
-        await this.getUsers();
+    const updateCoach = async () => {
+      try {
+        loading.value = true;
+        v$.value.selectedCoach.$touch();
+        if (v$.value.selectedCoach.$error) return NotifyWarning('Champ(s) invalide(s)');
+
+        await Users.updateById(selectedCoach.value._id, formatUpdatedUserPayload(selectedCoach.value));
+        coachEditionModal.value = false;
+        await getUsers();
         NotifyPositive('Compte modifié.');
       } catch (e) {
         console.error(e);
         NotifyNegative('Erreur lors de la modification du compte.');
       } finally {
-        this.loading = false;
+        loading.value = false;
       }
-    },
-    async getUsers () {
+    };
+
+    const getUsers = async () => {
       try {
-        this.usersLoading = true;
-        this.users = await Users.list({ role: [CLIENT_ADMIN, COACH], company: this.company._id });
+        usersLoading.value = true;
+        users.value = await Users.list({ role: [CLIENT_ADMIN, COACH], company: company.value._id });
       } catch (e) {
         console.error(e);
         NotifyNegative('Erreur lors de la récupération des comptes.');
-        this.users = [];
+        users.value = [];
       } finally {
-        this.usersLoading = false;
+        usersLoading.value = false;
       }
-    },
-    get,
+    };
+
+    const created = async () => {
+      await getUsers();
+    };
+
+    created();
+
+    return {
+      // Data
+      loading,
+      usersLoading,
+      coachCreationModal,
+      coachEditionModal,
+      firstStep,
+      usersColumns,
+      usersPagination,
+      newCoach,
+      selectedCoach,
+      users,
+      // Validations
+      v$,
+      // Computed
+      roleOptions,
+      // Methods
+      nextStep,
+      createCoach,
+      resetCoachCreationForm,
+      openCoachCreationModal,
+      openCoachEditionModal,
+      resetCoachEditionForm,
+      updateCoach,
+      get,
+      emailError,
+      phoneNbrError,
+    };
   },
 };
 </script>
