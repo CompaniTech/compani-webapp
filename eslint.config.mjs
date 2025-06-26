ad46d7c15d98911c47be43430dd9bfaa1aafb2ab
Manon Palin <manonpalin@mbp-de-manon.home>
2025-06-26 12:52:21 +0200

    COM-3997 - upgrade eslint-plugin-cypress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -68,13 +68,12 @@ export default defineConfig([
   globalIgnores(['dist', 'eslint.config.mjs', '.quasar', '.postcssrc.js', './test/cypress/support/component.js']),
   ...airbnb,
   ...compat.extends(
-    'plugin:cypress/recommended',
     'plugin:vue/vue3-strongly-recommended',
     'plugin:promise/recommended',
     'plugin:n/recommended'
   ),
   { // Configuration pour les fichiers .vue
-    plugins: { vue, cypress },
+    plugins: { vue },
     files: ['**/*.vue'],
     languageOptions: {
       parser: vueParser,
@@ -87,10 +86,22 @@ export default defineConfig([
       globals: {
         ...globals.browser,
         __statics: true,
-        'cypress/globals': true,
       },
     },
     rules,
+  },
+  { // Configuration pour les fichiers de tests e2e
+    files: ['test/cypress/**/*.{js,ts}', '**/*.spec.{js,ts}'],
+    plugins: { cypress },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...cypress.configs.recommended.languageOptions.globals,
+      },
+    },
+    rules: { ...cypress.configs.recommended.rules },
   },
   { // Configuration pour les fichiers .js et .mjs
     files: ['**/*.{js,mjs}'],
