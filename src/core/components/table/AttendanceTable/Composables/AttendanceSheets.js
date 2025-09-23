ee307c05f5de78ac97b01780ff357de86d9755a2
ulysse <ulysse@alenvi.io>
2025-09-23 14:23:02 +0200

    COM-4067: update attendances on attendance sheet edit

--- src/core/components/table/AttendanceTable/Composables/AttendanceSheets.js
+++ src/core/components/table/AttendanceTable/Composables/AttendanceSheets.js
@@ -26,7 +26,7 @@ export const useAttendanceSheets = (
   const attendanceSheetEditionModal = ref(false);
   const attendanceSheets = ref([]);
   const newAttendanceSheet = ref({ course: course.value._id });
-  const editedAttendanceSheet = ref({ _id: '', slots: [], trainee: {} });
+  const editedAttendanceSheet = ref({ _id: '', slots: [], trainee: {}, shouldUpdateAttendances: false });
   const editionSlotsGroupedByStep = ref({});
   const attendanceSheetColumns = ref([
     {
@@ -281,6 +281,7 @@ export const useAttendanceSheets = (
       _id: attendanceSheet._id,
       slots: linkedSlots.map(slot => slot._id),
       trainee: attendanceSheet.trainee,
+      shouldUpdateAttendances: true,
     };
 
     const groupedSlots = groupBy([...linkedSlots, ...notLinkedSlotOptions.value], 'step');
@@ -300,11 +301,13 @@ export const useAttendanceSheets = (
       if (v$.value.editedAttendanceSheet.$error) return NotifyWarning('Champs(s) invalide(s)');
       modalLoading.value = true;
 
-      await AttendanceSheets.update(editedAttendanceSheet.value._id, { slots: editedAttendanceSheet.value.slots });
+      const { slots, shouldUpdateAttendances } = editedAttendanceSheet.value;
+      await AttendanceSheets.update(editedAttendanceSheet.value._id, { slots, shouldUpdateAttendances });
 
       attendanceSheetEditionModal.value = false;
       NotifyPositive('Feuille d\'émargement modifiée.');
       await refreshAttendanceSheets();
+      if (shouldUpdateAttendances) await refreshAttendances({ course: course.value._id });
     } catch (e) {
       console.error(e);
       NotifyNegative('Erreur lors de l\'édition de la feuille d\'émargement.');
@@ -315,7 +318,7 @@ export const useAttendanceSheets = (
 
   const resetAttendanceSheetEditionModal = () => {
     v$.value.editedAttendanceSheet.$reset();
-    editedAttendanceSheet.value = { _id: '', slots: [], trainee: {} };
+    editedAttendanceSheet.value = { _id: '', slots: [], trainee: {}, shouldUpdateAttendances: false };
     editionSlotsGroupedByStep.value = {};
   };
 
