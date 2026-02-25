ebb5a6ad16f019f11dd068a02bc43d2557312833
Manon Palin <manonpalin@device-76.home>
2026-02-25 12:03:03 +0100

    COM-4148 - pagination style

--- src/modules/vendor/components/programs/SubProgramPriceVersionHistoryModal.vue
+++ src/modules/vendor/components/programs/SubProgramPriceVersionHistoryModal.vue
@@ -3,25 +3,25 @@
     <template #title>
       Historique <span class="text-weight-bold">des tarifs</span>
     </template>
-    <ni-responsive-table :data="subProgramPrices" :columns="subProgramPricesColumns"
-      no-data-label="Pas de tarifs renseignés pour ce sous-programme." />
+    <ni-responsive-table :data="subProgramPrices" :columns="subProgramPricesColumns" :hide-bottom="false"
+      no-data-label="Pas de tarifs renseignés pour ce sous-programme." v-model:pagination="pagination"
+      :rows-per-page-options="rowsPerPageOptions" />
   </ni-modal>
 </template>
 
 <script>
-import { toRefs, computed } from 'vue';
+import { toRefs, computed, ref } from 'vue';
+import CompaniDate from '@helpers/dates/companiDates';
+import { descendingSortBy } from '@helpers/dates/utils';
 import Modal from '@components/modal/Modal';
 import ResponsiveTable from '@components/table/ResponsiveTable';
 import { E_LEARNING, DD_MM_YYYY } from '@data/constants';
-import CompaniDate from '@helpers/dates/companiDates';
-import { descendingSortBy } from '@helpers/dates/utils';
 
 export default {
   name: 'SubProgramPriceVersionHistoryModal',
   props: {
     modelValue: { type: Boolean, default: false },
     subProgram: { type: Object, default: () => ({}) },
-    loading: { type: Boolean, default: false },
   },
   components: {
     'ni-modal': Modal,
@@ -30,6 +30,9 @@ export default {
   emits: ['hide', 'update:model-value', 'submit'],
   setup (props, { emit }) {
     const { subProgram } = toRefs(props);
+
+    const pagination = ref({ page: 1, rowsPerPage: 5 });
+    const rowsPerPageOptions = ref([5, 10]);
 
     const hide = () => { emit('hide'); };
     const input = (event) => { emit('update:model-value', event); };
@@ -60,10 +63,13 @@ export default {
     ]);
 
     return {
-      // computed
+      // Data
+      pagination,
+      rowsPerPageOptions,
+      // Computed
       subProgramPrices,
       subProgramPricesColumns,
-      // methods
+      // Methods
       hide,
       input,
       submit,
