97925bf12fd334470ffc07bb585695c8de7457c8
Manon Palin <manonpalin@mbp-de-manon.home>
2026-03-03 10:23:52 +0100

    COM-4149-2 - fix reviews

--- src/modules/vendor/components/billing/TrainerBillingInfosCard.vue
+++ src/modules/vendor/components/billing/TrainerBillingInfosCard.vue
@@ -4,28 +4,29 @@
       <template #header>
         <div class="row items-center full-width">
           <span class="text-copper-500">{{ formatIdentity(trainerInfos.identity, 'FL') }}</span>
-          <span class="text-weight-bold text-orange-400">
-            &nbsp;- à régler : {{ formattedTrainerDurations.notPaid }} (dont
+          <span v-if="displayDuration(formattedTrainerDurations.notPaid)" class="text-weight-bold text-orange-400">
+            &nbsp;- À régler : {{ formattedTrainerDurations.notPaid }} (dont
             &nbsp;{{ formattedTrainerDurations.notPaidAbsence }} d'absence)
           </span>
-          <span class="text-copper-500" v-if="formattedTrainerDurations.paid > 0">
+          <span v-if="displayDuration(formattedTrainerDurations.paid)" class="text-copper-500">
             &nbsp;/ réglé : {{ formattedTrainerDurations.paid }} (dont {{ formattedTrainerDurations.paidAbsence }}
             &nbsp;d'absence)
           </span>
         </div>
       </template>
       <div v-if="displayDetails" class="q-pa-sm bg-peach-200">
-        <q-expansion-item v-for="course of coursesWithComputedData" :key="course._id" class="q-ma-sm bg-white"
+        <q-expansion-item v-for="course of coursesWithFormattedData" :key="course._id" class="q-ma-sm bg-white"
           @click="showCourseDetails(course._id)">
           <template #header>
             <div class="full-width">
               <span class="text-weight-bold text-copper-600">{{ course.name }}</span>
-              <span class="text-weight-bold text-orange-400 q-ma-md">
+              <span v-if="displayDuration(course.notPaidSingleSlotsDuration)"
+                class="text-weight-bold text-orange-400">
                   <br> À régler : {{ course.notPaidSingleSlotsDuration }} (dont
                   &nbsp;{{ course.notPaidSingleSlotsAbsenceDuration }} d'absence)
                 </span>
-                <span class="text-copper-500" v-if="course.paidSingleSlotsDuration > 0">
-                  &nbsp;/ réglé : {{ course.paidSingleSlotsDuration }} (dont
+                <span class="text-copper-500" v-if="displayDuration(course.paidSingleSlotsDuration)">
+                  &nbsp;/&nbsp;réglé : {{ course.paidSingleSlotsDuration }} (dont
                   &nbsp;{{ course.paidSingleSlotsAbsenceDuration }} d'absence)
                 </span>
             </div>
@@ -34,12 +35,14 @@
             <div v-for="stepName of Object.keys(course.singleTraineeSlots)" :key="stepName" class="q-pa-sm q-pl-md">
               <span class="text-italic">{{ stepName }}</span>
               &nbsp;:&nbsp;
-              <span class="text-orange-400">
-                {{ course.totalsByStep[stepName].toPayDuration }} ({{ course.totalsByStep[stepName].toPayAmount }})
-                 à régler
+              <span class="text-orange-400" v-if="displayDuration(course.singleTraineeSlots[stepName].toPayDuration)">
+                {{ course.singleTraineeSlots[stepName].toPayDuration }} à régler
+                &nbsp;({{ course.singleTraineeSlots[stepName].toPayAmount }})
               </span>
-              <span v-if="course.totalsByStep[stepName].paid">
-                , {{ course.totalsByStep[stepName].paid }} ({{ course.totalsByStep[stepName].paidAmount }}) réglées.
+              <span v-if="displayDuration(course.singleTraineeSlots[stepName].paidDuration)">
+                <span v-if="displayDuration(course.singleTraineeSlots[stepName].toPayDuration)"> , </span>
+                {{ course.singleTraineeSlots[stepName].paidDuration }} réglées
+                &nbsp;({{ course.singleTraineeSlots[stepName].paidAmount }})
               </span>
             </div>
             <ni-expanding-table :data="course.rows" :columns="singleSlotColumns" />
@@ -49,13 +52,14 @@
           <template #header>
             <div class="full-width align-center">
               <span class="text-weight-bold text-copper-600"> Sessions collectives</span>
-              <span class="text-weight-bold text-orange-400 q-ma-md">
+              <span v-if="displayDuration(formattedCollectiveSlotsDurations.notPaid)"
+                class="text-weight-bold text-orange-400 q-ma-md">
                 <br> À régler : {{ formattedCollectiveSlotsDurations.notPaid }} (dont
                 &nbsp;{{ formattedCollectiveSlotsDurations.notPaidAbsence }} d'absence)
               </span>
-              <span class="text-copper-500" v-if="formattedCollectiveSlotsDurations.paid > 0">
+              <span class="text-copper-500" v-if="displayDuration(formattedCollectiveSlotsDurations.paid)">
                 &nbsp;/ réglé : {{ formattedCollectiveSlotsDurations.paid }} (dont
-                &nbsp;{{ formattedCollectiveSlotsDurations.notPaid }} d'absence)
+                &nbsp;{{ formattedCollectiveSlotsDurations.paidAbsence }} d'absence)
               </span>
             </div>
           </template>
@@ -78,11 +82,10 @@
 <script>
 
 import { ref, toRefs, computed } from 'vue';
-import { LONG_DURATION_H_MM, DD_MM_YYYY, HHhMM, PAID, NOT_PAID } from '@data/constants';
+import { LONG_DURATION_H_MM, DD_MM_YYYY, HHhMM } from '@data/constants';
 import { formatIdentity, formatStringToPrice } from '@helpers/utils';
 import CompaniDuration from '@helpers/dates/companiDurations';
 import CompaniDate from '@helpers/dates/companiDates';
-import { add, toFixedToFloat } from '@helpers/numbers';
 import ExpandingTable from '@components/table/ExpandingTable';
 import Banner from '@components/Banner';
 import { SLOT_STATUS } from '../../../../core/data/constants';
@@ -195,40 +198,10 @@ export default {
       },
     ]);
 
-    const coursesWithComputedData = computed(() => trainerInfos.value.courses.map((course) => {
+    const coursesWithFormattedData = computed(() => trainerInfos.value.courses.map((course) => {
       const singleSlots = Object.entries(course.singleTraineeSlots || {});
 
-      const rows = singleSlots.flatMap(([stepName, slots]) => slots.map(slot => ({ stepName, ...slot })));
-
-      const totalsByStep = {};
-      singleSlots.forEach(([stepName, slots]) => {
-        const total = slots.reduce(
-          (acc, slot) => {
-            if (slot.status === NOT_PAID) {
-              acc.toPayDuration = acc.toPayDuration.add(CompaniDuration(slot.duration));
-              acc.toPayAmount = add(acc.toPayAmount, slot.amount);
-            }
-            if (slot.status === PAID) {
-              acc.paidDuration = acc.paid.add(CompaniDuration(slot.duration));
-              acc.paidAmount = add(acc.paidAmount, slot.amount);
-            }
-            return acc;
-          },
-          {
-            toPayDuration: CompaniDuration('PT0S'),
-            paidDuration: CompaniDuration('PT0S'),
-            toPayAmount: 0,
-            paidAmount: 0,
-          }
-        );
-
-        totalsByStep[stepName] = {
-          toPayDuration: total.toPayDuration.format(LONG_DURATION_H_MM),
-          paidDuration: total.paidDuration.format(LONG_DURATION_H_MM),
-          toPayAmount: formatStringToPrice(toFixedToFloat(total.toPayAmount)),
-          paidAmount: formatStringToPrice(toFixedToFloat(total.paidAmount)),
-        };
-      });
+      const rows = singleSlots.flatMap(([stepName, obj]) => obj.slots.map(slot => ({ stepName, ...slot })));
 
       const {
         paidSingleSlotsDuration,
@@ -237,15 +210,28 @@ export default {
         notPaidSingleSlotsAbsenceDuration,
       } = course;
 
+      const formattedSingleTraineeSlots = Object.fromEntries(
+        singleSlots.map(([stepName, obj]) => [
+          stepName,
+          {
+            ...obj,
+            toPayDuration: CompaniDuration(obj.toPayDuration).format(LONG_DURATION_H_MM),
+            paidDuration: CompaniDuration(obj.paidDuration).format(LONG_DURATION_H_MM),
+            toPayAmount: formatStringToPrice(obj.toPayAmount),
+            paidAmount: formatStringToPrice(obj.paidAmount),
+          },
+        ])
+      );
+
       return {
         ...course,
+        singleTraineeSlots: formattedSingleTraineeSlots,
         paidSingleSlotsDuration: CompaniDuration(paidSingleSlotsDuration).format(LONG_DURATION_H_MM),
         paidSingleSlotsAbsenceDuration: CompaniDuration(paidSingleSlotsAbsenceDuration).format(LONG_DURATION_H_MM),
         notPaidSingleSlotsDuration: CompaniDuration(notPaidSingleSlotsDuration).format(LONG_DURATION_H_MM),
         notPaidSingleSlotsAbsenceDuration: CompaniDuration(notPaidSingleSlotsAbsenceDuration)
           .format(LONG_DURATION_H_MM),
         rows,
-        totalsByStep,
       };
     }));
 
@@ -278,13 +264,15 @@ export default {
       areCourseDetailsVisible.value[courseId] = !areCourseDetailsVisible.value[courseId];
     };
 
+    const displayDuration = value => value !== '0min';
+
     return {
       // Data
       displayDetails,
       areCourseDetailsVisible,
       // Computed
       singleSlotColumns,
-      coursesWithComputedData,
+      coursesWithFormattedData,
       collectiveSlotsColumns,
       formattedTrainerDurations,
       formattedCollectiveSlotsDurations,
@@ -292,6 +280,7 @@ export default {
       formatIdentity,
       showDetails,
       showCourseDetails,
+      displayDuration,
     };
   },
 };
