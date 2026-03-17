0ea96b7b04b8c650e3ae39bb08e6b749885e43a4
Manon Palin <manonpalin@macbook-pro-de-manon-1.home>
2026-03-17 13:01:02 +0100

    COM-4193 - displayDetails when not dashboard

--- src/modules/vendor/components/billing/TrainerBillingInfosCard.vue
+++ src/modules/vendor/components/billing/TrainerBillingInfosCard.vue
@@ -1,7 +1,7 @@
 <template>
   <q-card v-if="trainerInfos.courses.length || Object.keys(trainerInfos.collectiveSlots.slots).length"
     class="container clickable cursor-pointer" flat>
-    <q-expansion-item class="q-my-md">
+    <q-expansion-item class="q-my-md" v-model="displayDetails">
       <template #header>
         <div class="row items-center justify-between full-width">
           <div>
@@ -19,7 +19,7 @@
             @click.stop="openCourseSlotListValidationModal" :disabled="selectedCourseSlots.length === 0" />
         </div>
       </template>
-      <div class="q-pa-sm bg-peach-200">
+      <div v-if="displayDetails" class="q-pa-sm bg-peach-200">
         <q-expansion-item v-for="course of coursesWithFormattedData" :key="course._id" class="q-ma-sm bg-white"
           v-model="areCourseDetailsVisible[course._id]">
           <template #header>
@@ -166,6 +166,7 @@ export default {
     const { trainerInfos, trainerId, isTrainer } = toRefs(props);
     const $route = useRoute();
     const isDashboard = /\/trainers-follow-up/.test($route.path);
+    const displayDetails = ref(!isDashboard.value);
 
     const areCourseDetailsVisible = ref(
       Object.fromEntries(trainerInfos.value.courses.map(course => [course._id, false]))
@@ -419,6 +420,7 @@ export default {
       courseSlotsToPay,
       courseSlotListValidationModal,
       PAID,
+      displayDetails,
       // Validation
       v$,
       // Computed
