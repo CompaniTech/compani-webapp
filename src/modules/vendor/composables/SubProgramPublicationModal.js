11df219fd5d9e92ff7ecc7fbdb55121e67728048
ulysse <ulysse@alenvi.io>
2026-09-25 16:40:48 +0200

    COM-4289: can publish new elearning

--- src/modules/vendor/composables/SubProgramPublicationModal.js
+++ src/modules/vendor/composables/SubProgramPublicationModal.js
@@ -3,7 +3,7 @@ import { useQuasar } from 'quasar';
 import SubPrograms from '@api/SubPrograms';
 import Companies from '@api/Companies';
 import { NotifyNegative, NotifyWarning, NotifyPositive } from '@components/popup/notify';
-import { PUBLISHED, DIRECTORY } from '@data/constants';
+import { PUBLISHED, DIRECTORY, STRICTLY_E_LEARNING } from '@data/constants';
 import { formatAndSortCompanyOptions } from '@helpers/utils';
 
 export const useSubProgramPublicationModal = (program, refreshProgram) => {
@@ -13,19 +13,24 @@ export const useSubProgramPublicationModal = (program, refreshProgram) => {
   const subProgramPublicationModal = ref(false);
   const companyOptions = ref([]);
 
+  const hasReusableELearningCourse = subProgram => subProgram.isStrictlyELearning &&
+    program.value.subPrograms.some(
+      sp => sp._id !== subProgram._id && (sp.courses || []).some(c => c.format === STRICTLY_E_LEARNING)
+    );
+
   const checkPublicationAndOpenModal = async (subProgram) => {
     subProgramToPublish.value = subProgram;
 
-    const eLearningSubProgramAlreadyPublished = program.value.subPrograms.some(
-      sp => sp.isStrictlyELearning && sp._id !== subProgram._id && sp.status === PUBLISHED
+    const activeELearningSubProgram = program.value.subPrograms.some(
+      sp => sp.isStrictlyELearning && sp._id !== subProgram._id && sp.status === PUBLISHED && !sp.archivedAt
     );
-    if (subProgram.isStrictlyELearning && eLearningSubProgramAlreadyPublished) {
-      return NotifyWarning('Un programme ne peut contenir qu\'un seul sous programme eLearning publié.');
+    if (subProgram.isStrictlyELearning && activeELearningSubProgram) {
+      return NotifyWarning('Un programme ne peut contenir qu\'un seul sous-programme eLearning actif.');
     }
 
     if (!subProgram.areStepsValid) return NotifyWarning('Le sous-programme n\'est pas valide.');
 
-    return subProgram.isStrictlyELearning
+    return subProgram.isStrictlyELearning && !hasReusableELearningCourse(subProgram)
       ? openSubProgramPublicationModal()
       : validateSubProgramPublication();
   };
@@ -43,10 +48,16 @@ export const useSubProgramPublicationModal = (program, refreshProgram) => {
   };
 
   const validateSubProgramPublication = (accessCompanies = []) => {
+    const replaceArchivedELearningCourse = hasReusableELearningCourse(subProgramToPublish.value);
+
     $q.dialog({
       title: 'Confirmation',
-      message: 'Une fois le sous-programme publié, vous ne pourrez plus le modifier.<br />'
-        + 'Êtes-vous sûr(e) de vouloir publier ce sous-programme&nbsp;?',
+      message: `Une fois le sous-programme publié, vous ne pourrez plus le modifier.<br />${
+        replaceArchivedELearningCourse
+          ? 'Ce sous-programme remplacera le contenu eLearning actuellement archivé&nbsp;: les apprenants déjà '
+            + 'inscrits basculeront dessus et perdront leur progression sur l\'ancien contenu.<br />'
+          : ''
+      }Êtes-vous sûr(e) de vouloir publier ce sous-programme&nbsp;?`,
       html: true,
       ok: true,
       cancel: 'Annuler',
@@ -64,7 +75,7 @@ export const useSubProgramPublicationModal = (program, refreshProgram) => {
     } catch (e) {
       console.error(e);
       if (e.status === 409) {
-        return NotifyWarning('Un programme ne peut contenir qu\'un seul sous programme eLearning publié.');
+        return NotifyWarning('Un programme ne peut contenir qu\'un seul sous-programme eLearning publié.');
       }
 
       NotifyNegative('Erreur lors de la publication du sous-programme.');
